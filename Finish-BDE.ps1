04ec58a8874048ce750f6c6b69bfb875349af2ad
clutch70 <67942178+clutch70@users.noreply.github.com>
2023-05-19 10:39:00 -0500

    Remove script instructions to enhance compatibility with RMM tools.

--- Finish-BDE.ps1
+++ Finish-BDE.ps1
@@ -1,26 +1,3 @@
-########################################################################
-#																		
-#	Script Title: Finish-BDE											
-#	Author: Brennan Custard												
-#	Date: 8/26/2020														
-#	Description: This script accepts basic parameters to complete		
-#	the BDE process for a given OS disk in an endpoint.			 		
-#																		
-########################################################################
-#
-#	This script makes several assumptions.
-#		1. The password or PIN must be at least 8 or 6 characters respectively.
-#		2. If NewPIN is not specified, you want to use the last 6 of the 
-#			serial number (all caps) as	the first PIN.
-#		3. If NewPassword is not specified, you want to use a provided 
-#			CompanyIdentifier paramater plus the last 6 of the serial
-#			number (all caps) as the first password.
-#		4. By default a RecoveryPasswordProtector should be added.
-#		5. By default a hardware test will be performed before encryption
-#
-########################################################################
-
-
 param ($NewPIN, $NewPassword, $CreateRecoveryPassword=$true, $CompanyIdentifier, [Switch]$NoHardwareTest, [Switch]$RmmTool, [Switch]$Verbose, [Switch]$Testing, [Switch]$DisplayCredential, [Switch]$TPMProtectorOnly, $BDEEncryptionMethod="AES256", [Switch]$SkipAdBackup)
 
 	
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Finish-BDE
  Accepts parameters to complete BDE in one script.
+
+########################################################################
+#																		
+	Script Title: Finish-BDE											
+	Author: Brennan Custard												
+	Date: 8/26/2020														
+	Description: This script accepts basic parameters to complete		
+	the BDE process for a given OS disk in an endpoint.			 		
+																		
+########################################################################
+#
+#	This script makes several assumptions.
+		1. The password or PIN must be at least 8 or 6 characters respectively.
+		2. If NewPIN is not specified, you want to use the last 6 of the 
+			serial number (all caps) as	the first PIN.
+		3. If NewPassword is not specified, you want to use a provided 
+			CompanyIdentifier paramater plus the last 6 of the serial
+			number (all caps) as the first password.
+		4. By default a RecoveryPasswordProtector should be added.
+		5. By default a hardware test will be performed before encryption
+#
+########################################################################
